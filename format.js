d984ea71bf13122bbc424ec560629be8b74b5769
Tim Ryan <id@timryan.org>
2017-01-29 21:12:33 -0800

    Removes comma formatting of names.

--- format.js
+++ format.js
@@ -1,3 +1,11 @@
 var reps = require('./representatives');
 
+// console.log(reps);
+
+for (var i = 0; i < reps.length; i++) {
+  if (reps[i].name.indexOf(',') > -1) {
+    reps[i].name = reps[i].name.replace(/^(.*?),(?! Jr.?)\s*(.*?)(,.*?| Jr.?)?$/, '$2 $1$3');
+  }
+}
+
 console.log(JSON.stringify(reps, null, 2))
